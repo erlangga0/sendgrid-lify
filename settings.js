323b7b5c7f2b70642b3889dfd993cd77e2ce4c07
erlangga0 <erlangga.maulana@gmail.com>
2019-12-19 19:04:15 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+// Change the url to the domain of yourapp
+exports.url = 'https://sendgridlify.herokuapp.com';
+exports.senderEmail = "erlangga.maulana@gmail.com";
+exports.senderName = "Erlangga Maulana";
+
+// set 'exports.listId = null' to add contact to all contacts, but no specific list
+// or a string with the listId to add to a specific list
+exports.listId = null;
+
+// set 'exports.templateId = null' to opt out of using a template
+// or a string with the templateId to use a template
+exports.templateId = null;
+
+// receive an email when a new signup is confirmed
+exports.sendNotification = true;
+exports.notificationEmail = 'admin@example.com';
